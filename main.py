780dedc69a1b431ff32926824c834ee99dd97bd6
Tomhausen <>
2021-06-05 17:34:47 +0000

    Added new sprites and spawned aliens

---
+++ main.py
@@ -0,0 +1,68 @@
+from pgzrun import go
+from random import randint
+
+
+TITLE = "SPACE INVADERS"
+WIDTH = 750
+HEIGHT = 600
+
+
+# actors
+ship = Actor("ship", (WIDTH//2, HEIGHT - 21))
+pips = []
+aliens = []
+alien_costumes = ["alien1", "alien2", "alien3", "alien4"]
+
+
+def update(): # built in
+    ship_movement()
+    update_pips()
+
+
+def draw(): # built in
+    screen.blit("background", (0, 0))
+    ship.draw()
+""" for pip in pips:
+        pip.draw()
+    for alien in aliens:
+        alien.draw()"""
+
+
+def ship_movement():
+    if keyboard[keys.LEFT]:
+        ship.x -= 5
+    elif keyboard[keys.RIGHT]:
+        ship.x += 5
+
+
+def on_key_down(key): # built in
+    if key == key.SPACE:
+        pips.append(Actor("pip", (ship.x, ship.y - 15)))
+
+    
+def update_pips():
+    index = 0
+    for pip in pips:
+        pip.y -= 10
+        if pip.y < 0:
+            pips.pop(index)    
+        index += 1
+
+
+def spawn_aliens():
+    y_spawn = 75
+    columns = 0
+    while columns < 4:
+        row_length = 0
+        x_spawn = 150
+        while row_length < 10:
+            alien = Actor(alien_costumes[columns], (x_spawn, y_spawn))
+            aliens.append(alien)
+            row_length += 1
+            x_spawn += 50
+        columns += 1
+        y_spawn += 50
+
+
+#spawn_aliens()
+go()
